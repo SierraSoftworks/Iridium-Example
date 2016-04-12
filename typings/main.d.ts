62957b79c3747d10ac7a6789838b990b8a76dde1
Benjamin Pannell <admin@sierrasoftworks.com>
2016-04-12 22:29:49 +0200

    tweak: Remove iridium typings reference and rely on node module resolution

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,5 +1,3 @@
 /// <reference path="main/ambient/es6-promise/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/ambient/promises-a-plus/index.d.ts" />
 /// <reference path="main/definitions/bluebird/index.d.ts" />
-/// <reference path="main/definitions/iridium/index.d.ts" />
