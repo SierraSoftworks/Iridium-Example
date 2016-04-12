311bd34feb4347db2976db92abbc751f05113703
Benjamin Pannell <admin@sierrasoftworks.com>
2016-04-12 14:29:44 +0200

    feat: Add typings type definitions

---
+++ typings/main.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="main/ambient/es6-promise/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
+/// <reference path="main/definitions/bluebird/index.d.ts" />
+/// <reference path="main/definitions/lodash/index.d.ts" />
+/// <reference path="main/definitions/mongodb/index.d.ts" />
