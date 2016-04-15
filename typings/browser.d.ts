df45f6fd6274a4e12632ff8790725015e850fbf7
Benjamin Pannell <admin@sierrasoftworks.com>
2016-04-15 18:49:53 +0200

    fix: Showcase generation for ES6 with node module references

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="browser/ambient/es6-promise/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
 /// <reference path="browser/definitions/bluebird/index.d.ts" />
+/// <reference path="browser/definitions/lodash/index.d.ts" />
+/// <reference path="browser/definitions/mongodb/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,4 @@
-/// <reference path="main/ambient/es6-promise/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="main/definitions/bluebird/index.d.ts" />
+/// <reference path="main/definitions/lodash/index.d.ts" />
+/// <reference path="main/definitions/mongodb/index.d.ts" />
