8222ee4208f45268bb52a88e9af0c6c57b31a882
Benjamin Pannell <admin@sierrasoftworks.com>
2016-07-29 13:37:38 +0200

    refactor: Switch to TypeScript 2.0

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/es6-promise/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/definitions/bluebird/index.d.ts" />
-/// <reference path="browser/definitions/lodash/index.d.ts" />
-/// <reference path="browser/definitions/mongodb/index.d.ts" />
